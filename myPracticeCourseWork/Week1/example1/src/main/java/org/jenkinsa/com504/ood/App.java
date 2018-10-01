fd828a38ea0c6f56fcfcbc2cd27d7a125804f360
Aaron Jenkins <JENKA17@solent.ac.uk>
2018-10-01 12:01:07 +0100

    example1 loaded into intellij

---
+++ myPracticeCourseWork/Week1/example1/src/main/java/org/jenkinsa/com504/ood/App.java
@@ -0,0 +1,13 @@
+package org.jenkinsa.com504.ood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
